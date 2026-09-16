e3c5995a292e23971d46894a37e4c2d76bd44077
bryanmsombra27 <bryanjm96@gmail.com>
2026-09-15 20:44:34 -0600

    adding filters and paginations

--- minilibrary/views.py
+++ minilibrary/views.py
@@ -3,28 +3,38 @@ from .models import Book
 from django.db.models import Q
 # Create your views here.
 # minilibrary\views.py
+from django.core.paginator import Paginator
 
 
 def index(request):
     books = Book.objects.all()
     query = request.GET.get("query_search")
+    date_start = request.GET.get("start")
+    date_end = request.GET.get("end")
 
     if query:
         books = books.filter(
             Q(title__icontains=query) |
             Q(author__name__icontains=query)
         )
-    # author_id = request.GET.get("author")
-    # genre_id = request.GET.get("genre")
 
-    # if author_id:
-    #     books = books.filter(author_id=author_id)
+    if date_start and date_end:
+        books = books.filter(publication_date__range=[date_start, date_end])
 
-    # if genre_id:
-    #     books = books.filter(genres__id=genre_id)
+    query_params = request.GET.copy()
+
+    if "page" in query_params:
+        query_params.pop("page")
+
+    query_string = query_params.urlencode()
+
+    paginator = Paginator(books, 5)
+    page_number = request.GET.get("page")
+    page_obj = paginator.get_page(page_number)
 
     return render(request, 'minilibrary/index.html', {
-        "books": books,
-        "query": query
+        "page_obj": page_obj,
+        "query": query,
+        "query_string": query_string
 
     })
